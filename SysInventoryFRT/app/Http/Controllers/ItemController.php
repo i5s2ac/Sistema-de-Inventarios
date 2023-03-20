a2b3dbb561f3ffb15f42effbf7950519a4787e40
i5s2ac <103969157+i5s2ac@users.noreply.github.com>
2023-03-20 13:48:26 -0600

    CRUD app basic model

---
+++ SysInventoryFRT/app/Http/Controllers/ItemController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $items = Item::latest()->paginate(5);
+
+        return view('items.index',compact('items'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('items.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+
+        Item::create($request->all());
+
+        return redirect()->route('items.index')
+                        ->with('success','Item created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Item $item)
+    {
+        return view('items.show',compact('item'));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Item $item)
+    {
+        return view('items.edit',compact('item'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Item $item)
+    {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+
+        $item->update($request->all());
+
+        return redirect()->route('items.index')
+                        ->with('success','Product updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Item $item)
+    {
+        $item->delete();
+
+        return redirect()->route('items.index')
+                        ->with('success','Product deleted successfully');
+    }
+}
---
+++ SysInventoryFRT/resources/views/items/show.blade.php
@@ -0,0 +1,29 @@
+@extends('items.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show item</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('items.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $item->name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Details:</strong>
+                {{ $item->detail }}
+            </div>
+        </div>
+    </div>
+@endsection
