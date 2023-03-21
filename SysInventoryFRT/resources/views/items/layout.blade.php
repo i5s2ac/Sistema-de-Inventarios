5fa8877c5759b81c5ef581f031d524be13562344
i5s2ac <103969157+i5s2ac@users.noreply.github.com>
2023-03-20 18:44:15 -0600

    Side-Bar implementation
    Aún faltan los detalles, se continuará trabajando más adelante.

--- SysInventoryFRT/resources/views/items/layout.blade.php
+++ SysInventoryFRT/resources/views/items/layout.blade.php
@@ -4,14 +4,118 @@
 <head>
     <title>Laravel 9 CRUD Application - ItSolutionStuff.com</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .sidebar {
+            height: 100%;
+            width: 220px;
+            position: absolute;
+            top: 0;
+            left: 0;
+            background-color: #343a40;
+            overflow-x: hidden;
+            padding-top: 20px;
+        }
+
+        .sidebar a {
+            display: block;
+            color: white;
+            padding: 10px;
+            text-decoration: none;
+        }
+
+        .sidebar a:hover {
+            background-color: #616161;
+        }
+
+        .sidebar .active {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        .col-9 {
+            flex: 1;
+            padding-left: 270px;
+            padding-right: 40px;
+        }
+
+        .sidebar .nav-link {
+            width: 100%;
+        }
+    </style>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
+
 </head>
 
 <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark sidebar">
+                <div class="d-flex flex-column flex-shrink-0 p-3">
 
-    <div class="container">
-        @yield('content')
-    </div>
+                    <img src="{{ asset('img/FRT.png') }}" alt="Logo" width="160" height="55">
 
+
+                    <hr>
+                    <ul class="nav nav-pills flex-column mb-auto">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active" aria-current="page">
+                                <i class="fas fa-home me-2"></i>
+
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link text-white">
+                                <i class="fas fa-tachometer-alt me-2"></i>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('items.index') }}" class="nav-link text-white">
+                                <i class="fas fa-box me-2"></i>
+                                Inventario
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link text-white">
+                                <i class="fas fa-truck me-2"></i>
+                                Proveedores
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link text-white">
+                                <i class="fas fa-users me-2"></i>
+                                Empleados
+                            </a>
+                        </li>
+                    </ul>
+                    <hr>
+                    <div class="dropdown">
+                        <a href="#"
+                            class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                            id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="https://github.com/mdo.png" alt="" width="32" height="32"
+                                class="rounded-circle me-2">
+                            <strong>mdo</strong>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                            <li><a class="dropdown-item" href="#">New project...</a></li>
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Sign out</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                @yield('content')
+            </div>
 </body>
 
 </html>
